bb66a93675c314d47e0f035f339d0dda3a1dcd86
Elvira Sumarokava <40741867+eltimka@users.noreply.github.com>
2023-11-27 10:14:06 -0500

    Added Post Request

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws URISyntaxException {
 		SpringApplication.run(DemoApplication.class, args);
+
+        Transcript transcript = new Transcript();
+		transcript.setAudio_url("https:// audio path url ");
+		Gson gson = new Gson();
+		String jsonRequest = gson.toJson(transcript);
+
+		System.out.println(jsonRequest);
+
+		HttpRequest postRequest = HttpRequest.newBuilder()
+				.uri(new URI("https://api.assemblyai.com/v2/transcript"))
+				.header("Autorization", Constants.API_KEY)
+				.POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+				.build();
+
+		HttpClient httpClient = HttpClient.newHttpClient();
+
+		HttpResponse<String> postResponse = httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+
+		System.out.println(postResponse.body());
+
+
 	}
 
 }
